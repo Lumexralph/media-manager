bc35cc240665a22efc27f5159cb37aec065b6f9f
Olumide Ogundele <olumideralph@gmail.com>
2020-02-14 10:49:52 +0100

    implement the service layer

--- media-manager.ts
+++ media-manager.ts
@@ -1,4 +1,10 @@
 import localForage from "localforage";
+import "reflect-metadata";
+import {
+    classToPlain,
+    plainToClassFromExist,
+    Expose, Type
+} from "class-transformer";
 
 
 enum Genre {
@@ -30,6 +36,7 @@ abstract class Media {
 
     // since we've private fields, we access them using the accessors
     // getters and setters
+    @Expose()
     get identifier(): string {
         return this._identifier;
     }
@@ -38,6 +45,7 @@ abstract class Media {
         this._identifier = identifier;
     }
 
+    @Expose()
     get name(): string {
         return this._name;
     }
@@ -46,6 +54,7 @@ abstract class Media {
         this._name = name;
     }
 
+    @Expose()
     get description(): string {
         return this._description;
     }
@@ -54,6 +63,16 @@ abstract class Media {
         this._description = description;
     }
 
+    @Expose()
+    get pictureLocation(): string {
+        return this._pictureLocation;
+    }
+
+    set pictureLocation(pictureLocation: string) {
+        this._pictureLocation = pictureLocation;
+    }
+
+    @Expose()
     get genre(): Genre {
         return this._genre;
     }
@@ -88,6 +107,7 @@ class Book extends Media {
         this._numberOfPages = numberOfPages;
     }
 
+    @Expose()
     get author(): string {
         return this._author;
     }
@@ -96,6 +116,8 @@ class Book extends Media {
         this._author = author;
     }
 
+    @Expose()
+    @Type(() => Number)
     get numberOfPages(): number {
         return this._numberOfPages;
     }
@@ -142,6 +164,7 @@ class Movie extends Media {
         this._imdbRating = imdbRating;
     }
 
+    @Expose()
     get director(): string {
         return this._director;
     }
@@ -150,6 +173,7 @@ class Movie extends Media {
         this._director = director;
     }
 
+    @Expose()
     get duration(): string {
         return this._duration;
     }
@@ -158,6 +182,7 @@ class Movie extends Media {
         this._duration = duration;
     }
 
+    @Expose()
     get imdbRating(): IMDBRating {
         return this._imdbRating;
     }
@@ -172,14 +197,14 @@ class MediaCollection<T extends Media> {
     private _identifier: string;
     private _name: string = "";
     private _collection: ReadonlyArray<T> = [];
-    private readonly type: Function;
+    private readonly _type: Function;
 
     constructor(
         type: Function,
         name?: string,
         identifier?: string,
     ) {
-        this.type = type;
+        this._type = type;
 
         if (name) {
             this._name = name;
@@ -193,6 +218,7 @@ class MediaCollection<T extends Media> {
         }
     }
 
+    @Expose()
     get identifier(): string {
         return this._identifier;
     }
@@ -201,6 +227,7 @@ class MediaCollection<T extends Media> {
         this._identifier = identifier;
     }
 
+    @Expose()
     get name(): string {
         return this._name;
     }
@@ -209,6 +236,14 @@ class MediaCollection<T extends Media> {
         this._name = name;
     }
 
+    @Expose()
+    @Type(options => {
+        if (options) {
+            return (options.newObject as MediaCollection<T>)._type
+        } else {
+            throw new Error("cannot determine the type because the options object is null or undefined");
+        }
+    })
     get collection(): ReadonlyArray<T> {
         return this._collection;
     }
@@ -228,6 +263,7 @@ class MediaCollection<T extends Media> {
             });
         }
     }
+
 }
 
 // isolate persistence concerns in the service layer by implementing the service interface
@@ -254,5 +290,94 @@ class MediaServiceImpl<T extends Media> implements MediaService<T> {
             storeName: `media-manager-${_type.name}`,
             description: "MediaManager data store",
         })
+    }
+
+    saveMediaCollection(collection: Readonly<MediaCollection<T>>): Promise<void> {
+        // there won't be a value coming out of the promise upon resolution
+        // (that is, it will just be a signal).
+        return new Promise((resolve, reject) => {
+            if (!collection) {
+                reject(new Error("The collection cannot be null or undefined"));
+            }
+
+            console.log(`saving media collection with the name ${collection.name}`);
+            // We use classToPlain to serialize the MediaCollection object:
+            // Note that we use the excludePrefixes option in order to exclude all properties
+            // whose name starts with an underscore in order to avoid exposing our private
+            // properties directly (for example, _name in the Media class).
+            const serializedVersion = classToPlain(collection, {
+                excludePrefixes: ["_"]
+            });
+
+            this._store.setItem(collection.identifier, serializedVersion)
+                .then(value => {
+                    console.log(`saved the ${collection.name} collection successfully
+                    Saved value: ${value}`);
+                    resolve();
+                })
+                .catch(err => {
+                    console.error(`Failed to save the ${collection.name}
+                    collection with identifier ${collection.identifier}.
+                    Error: ${err}`)
+                    reject(err);
+                })
+        });
+    }
+
+    loadMediaCollection(identifier: string): Promise<MediaCollection<T>> {
+        console.log(`trying to load the media collection with identifier ${identifier}`);
+
+        return new Promise((resolve, reject) => {
+            this._store.getItem(identifier)
+                .then(value => {
+                    console.log(`found the collection: ${value} `);
+
+                    // deserialize the data
+                    const retrievedCollection = plainToClassFromExist<MediaCollection<T>, any>(new MediaCollection<T>(this._type), value);
+
+                    console.log(`retrieved collection: ${retrievedCollection}`);
+                    resolve(retrievedCollection);
+                })
+                .then(err => {
+                    reject(err);
+                });
+        });
+    }
+
+    getMediCollectionIdentifiersList(): Promise<string[]> {
+        return new Promise<string[]>((resolve, reject) => {
+            console.log("retrieving list of media collection identifiers");
+
+            this._store.keys()
+                .then(keys => {
+                    console.log(`successfully retrieved the media collection
+                    identifiers: ${keys}`);
+                    resolve(keys);
+                })
+                .catch(err => {
+                    console.log(`Failed to retrieve the list of media collection identifiers.
+                         Error: ${err}`);
+                    reject(err);
+                });
+        });
+    }
+
+    removeMediaCollection(identifier: string): Promise<void> {
+        return new Promise<void>((resolve, reject) => {
+            if (!identifier || identifier.trim() === "") {
+                reject(new Error("identifier must be provided"));
+            }
+
+            console.log(`removing this identifier ${identifier} from media collections`);
+            this._store.removeItem(identifier)
+                .then(() => {
+                    console.log("identifier removed successfully");
+                    resolve();
+                })
+                .catch(err => {
+                    console.error("failed to remove the identifier");
+                    reject(err);
+                });
+        });
     }
 }
